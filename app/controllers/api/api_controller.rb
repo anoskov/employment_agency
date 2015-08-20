e9d85a974bdf7ae39017258efd29a31ad655a6aa
a.noskov <flashbulb54@gmail.com>
2015-08-20 19:40:59 +0300

    a little fix

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -2,9 +2,6 @@ module Api
 
   class ApiController < ActionController::API
 
-    rescue_from ActiveRecord::RecordNotFound,
-                :with => -> { render :partial => 'api/v1/layouts/not_found', :status => 404 }
-
   end
 
 end
